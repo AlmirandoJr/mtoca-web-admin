bfde8da4525d9c99558372570127890ae88d480d
cfaife <clerio.faife@gmail.com>
2020-07-01 13:18:09 +0200

    add configs for deployments

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(_dirname+'/dist/mtoca-web-admin'))
+app.use(express.static(_dirname + '/dist/mtoca-web-admin'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(_dirname+'/dist/mtoca-web-admin/index.html'));
+    res.sendFile(path.join(_dirname + '/dist/mtoca-web-admin/index.html'));
 });
-app.listen(process.env || 8080);
+app.listen(process.env.PORT || 8080);
